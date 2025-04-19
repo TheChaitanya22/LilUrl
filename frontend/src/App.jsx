774e9ff202d14e9c36d868752ee2237dd2a01ba6
Chaitanya Goratela <officialchaitanya22@gmail.com>
2025-04-19 19:57:56 +0530

    fixed home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes,} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes,} from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import Home from "./pages/Home";
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to={"/home"} />} />
         <Route path="/home" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
